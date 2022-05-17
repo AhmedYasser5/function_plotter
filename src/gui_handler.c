395e2bcb1e89330663f6bbc57a4acbc405db9575
Ahmed Yasser <ahmedyasserfarouk6@gmail.com>
2022-05-17 18:56:22 +0200

    added some comments and improved the plotting of horizontal lines

--- src/gui_handler.c
+++ src/gui_handler.c
@@ -62,6 +62,7 @@ static gdouble convert_from_display(gdouble p, const gdouble *min,
 
 /* tracks the mouse in the helper */
 static gboolean mouse_tracking(gui_handler *handler) {
+  // make sure the helper is turned on
   if (handler->eq == NULL || !handler->helperActive)
     return FALSE;
 
@@ -71,19 +72,28 @@ static gboolean mouse_tracking(gui_handler *handler) {
           *minY = &handler->minY, *maxY = &handler->maxY;
   gdouble *helperX = &handler->helperX, *helperY = &handler->helperY;
 
-  gdouble x = convert_from_display(*helperX - HELPER_DX, minX, maxX, WIDTH);
+  // we should subtract the border's width "HELPER_DX"
+  gdouble approxX =
+      convert_from_display(*helperX - HELPER_DX, minX, maxX, WIDTH);
   gdouble step = PRECISION * (*maxX - *minX) / WIDTH;
-  int index = (x - *minX) / step;
-  gdouble approxX = *minX + index * step, approxY = handler->points[index];
+  // approximating the location of the point that the mouse is pointing at
+  int index = (approxX - *minX) / step;
+  gdouble approxY = handler->points[index];
+
+  // handling out-of-bounds points
   if (isless(approxX, *minX) || isless(*maxX, approxX)) {
     approxX = approxY = NAN;
     *helperX = NAN;
   }
+
   if (isnan(approxY))
     *helperY = NAN;
   else
     *helperY =
-        HEIGHT - convert_to_display(approxY, minY, maxY, HEIGHT) + HELPER_DY;
+        HEIGHT - convert_to_display(approxY, minY, maxY, HEIGHT) +
+        HELPER_DY; // the y-axis is inverted since it starts from the top.. we
+                   // should also add "HELPER_DY" to avoid drawing at the border
+
   gtk_widget_queue_draw(handler->helper);
   sprintf(message, "(x, y) = (%lf, %lf)", approxX, approxY);
   print_label_info(messages, message);
@@ -93,8 +103,10 @@ static gboolean mouse_tracking(gui_handler *handler) {
 /* tracks mouse clicks in the helper */
 gboolean on_helper_button_press_event(GtkWidget *widget, GdkEventButton *event,
                                       gpointer data) {
+  // make sure it is a left click of the mouse
   if (event->button != 1)
     return FALSE;
+
   gui_handler *handler = data;
   handler->helperX = event->x;
   return mouse_tracking(handler);
@@ -104,9 +116,13 @@ gboolean on_helper_button_press_event(GtkWidget *widget, GdkEventButton *event,
 gboolean on_helper_motion_notify_event(GtkWidget *widget, GdkEventMotion *event,
                                        gpointer data) {
   gui_handler *handler = data;
+  // make sure it is a left click of the mouse, and the last point is somewhere
+  // far from this new point (this decreases the load on the cpu so that it does
+  // not draw on every little move of the mouse)
   if (!(event->state & GDK_BUTTON1_MASK) ||
       isless(fabs(handler->helperX - event->x), PRECISION))
     return FALSE;
+
   handler->helperX = event->x;
   return mouse_tracking(handler);
 }
@@ -114,18 +130,23 @@ gboolean on_helper_motion_notify_event(GtkWidget *widget, GdkEventMotion *event,
 /* draws moving axes to identify the point on the graph */
 gboolean on_helper_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
   gui_handler *handler = data;
+  // make sure the helper is turned on
   if (isnan(handler->helperX) || !handler->helperActive)
     return FALSE;
+
   cairo_set_line_width(cr, LINE_WIDTH / 3.0);
   cairo_set_source_rgb(cr, 1 - RED, 1 - GREEN, 1 - BLUE);
+
   if (!isnan(handler->helperX)) {
     cairo_move_to(cr, handler->helperX, 0);
     cairo_line_to(cr, handler->helperX, HEIGHT + 2 * HELPER_DY);
   }
+
   if (!isnan(handler->helperY)) {
     cairo_move_to(cr, 0, handler->helperY);
     cairo_line_to(cr, WIDTH + 2 * HELPER_DX, handler->helperY);
   }
+
   cairo_stroke(cr);
   return TRUE;
 }
@@ -134,25 +155,35 @@ gboolean on_helper_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
 gboolean on_grid_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
   gui_handler *handler = data;
 
+  // make sure the grid is turned on
   if (handler->gridActive) {
+    // the whole height and width:
     gdouble height = HEIGHT + 2 * HELPER_DY, width = WIDTH + 2 * HELPER_DX;
     cairo_set_line_width(cr, LINE_WIDTH / 3.0);
     cairo_set_source_rgba(cr, 1, 1, 1, 0.75);
+
+    // drawing x-axis ticks
     for (gdouble x = 0, step = width / 8.0; x <= width; x += step) {
       cairo_move_to(cr, x, height);
       cairo_line_to(cr, x, height - HELPER_DY);
     }
+
+    // drawing y-axis ticks
     for (gdouble y = 0, step = height / 6.0; y <= height; y += step) {
       cairo_move_to(cr, 0, height - y);
       cairo_line_to(cr, HELPER_DX, height - y);
     }
+
     cairo_stroke(cr);
 
+    // if there is no equation, don't continue
     if (handler->eq == NULL)
       return TRUE;
 
     cairo_set_font_size(cr, FONT_SIZE);
     char *coordinates = (char *)malloc(sizeof(char) * 10);
+
+    // write down x-axis numbers
     for (gdouble x = 0, step = width / 8.0; x <= width; x += step) {
       int num = sprintf(coordinates, "%.2e",
                         convert_from_display(x - HELPER_DX, &handler->minX,
@@ -160,6 +191,8 @@ gboolean on_grid_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
       cairo_move_to(cr, x - num * FONT_SIZE / 4.0, height - 2 * HELPER_DY);
       cairo_text_path(cr, coordinates);
     }
+
+    // write down y-axis numbers
     for (gdouble y = 0, step = height / 6.0; y <= height; y += step) {
       sprintf(coordinates, "%.2e",
               convert_from_display(y - HELPER_DY, &handler->minY,
@@ -167,6 +200,7 @@ gboolean on_grid_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
       cairo_move_to(cr, 2 * HELPER_DX, height - y + FONT_SIZE / 4.0);
       cairo_text_path(cr, coordinates);
     }
+
     cairo_fill(cr);
     free(coordinates);
   }
@@ -189,11 +223,15 @@ gboolean on_graph_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
   gdouble cur = *minX, step = PRECISION * (*maxX - *minX) / WIDTH;
 
   for (int i = 0, n = WIDTH / PRECISION; i + 1 < n; i++, cur += step) {
+
     if (!isnan(p[i]) && !isnan(p[i + 1])) {
+      // we should subtract from "HEIGHT" since the y-axis is inverted
       cairo_move_to(cr, convert_to_display(cur, minX, maxX, WIDTH),
                     HEIGHT - convert_to_display(p[i], minY, maxY, HEIGHT));
+
       cairo_line_to(cr, convert_to_display(cur + step, minX, maxX, WIDTH),
                     HEIGHT - convert_to_display(p[i + 1], minY, maxY, HEIGHT));
+
       cairo_stroke(cr);
     }
   }
@@ -207,6 +245,7 @@ static char set_min_max(GtkWidget *min, GtkWidget *max, gdouble *min_co,
                         gchar *message) {
   char state = 0;
 
+  // read Minimum x or y
   const gchar *tmp = gtk_entry_get_text(GTK_ENTRY(min));
   *min_co = calculator_atolf(tmp);
   if (isnan(*min_co)) {
@@ -215,6 +254,7 @@ static char set_min_max(GtkWidget *min, GtkWidget *max, gdouble *min_co,
       state *= -1;
   }
 
+  // read Maximum x or y
   tmp = gtk_entry_get_text(GTK_ENTRY(max));
   *max_co = calculator_atolf(tmp);
   if (state <= 0 && isnan(*max_co)) {
@@ -225,18 +265,22 @@ static char set_min_max(GtkWidget *min, GtkWidget *max, gdouble *min_co,
       state = 2;
   }
 
+  // make sure Minimum x or y is less than Maximum x or y
   if (!state && isless(*max_co, *min_co)) {
     state = 3;
     *min_co = *max_co = NAN;
   }
 
+  // make sure Minimum x or y is not equal to Maximum x or y
   if (!state && isgreaterequal(*min_co, *max_co) &&
       isgreaterequal(*max_co, *min_co)) {
     state = 4;
     *min_co = *max_co = NAN;
   }
 
+  // report min and max for debugging
   printf("min_%c = %lf, max_%c = %lf\n", c, *min_co, c, *max_co);
+  // handling errors
   if (state) {
     if (abs(state) == 1)
       sprintf(message, "Invalid Minimum %c", c);
@@ -248,6 +292,7 @@ static char set_min_max(GtkWidget *min, GtkWidget *max, gdouble *min_co,
       sprintf(message, "Minimum %c cannot be equal to Maximum %c", c, c);
     print_label_error(messages, message);
   }
+
   return state;
 }
 
@@ -263,9 +308,13 @@ void on_draw_clicked(GtkButton *draw, gpointer data) {
   GtkWidget *messages = handler->messages;
   gchar *message = handler->message;
 
+  // errors of x values are intolerable, while empty y values are allowed
   if (set_min_max(min_x, max_x, minX, maxX, 'x', messages, message) ||
       set_min_max(min_y, max_y, minY, maxY, 'y', messages, message) > 0)
     return;
+
+  // setting auto y values; for example, if isMaxYNan is true, the maxY will be
+  // set to auto
   gboolean isMaxYNan = FALSE, isMinYNan = FALSE;
   if (isnan(*minY)) {
     isMinYNan = TRUE;
@@ -277,6 +326,7 @@ void on_draw_clicked(GtkButton *draw, gpointer data) {
   }
 
   const gchar *originalEq = gtk_entry_get_text(GTK_ENTRY(handler->equation));
+
   if (handler->eq)
     free(handler->eq);
   handler->eq = (char *)malloc(sizeof(char) * strlen(originalEq) + 1);
@@ -284,6 +334,7 @@ void on_draw_clicked(GtkButton *draw, gpointer data) {
   gchar *eq = handler->eq;
   strcpy(eq, originalEq);
   printf("equation = %s\n", eq);
+
   if (!strcmp(eq, "")) {
     print_label_error(messages, "Equation cannot be empty");
     return;
@@ -293,18 +344,30 @@ void on_draw_clicked(GtkButton *draw, gpointer data) {
   gdouble *p = handler->points;
   gdouble step = PRECISION * (*maxX - *minX) / WIDTH, cur = *minX;
   for (int i = 0, n = WIDTH / PRECISION; i < n; i++, cur += step) {
+    // calculating the corresponding y for x = cur
     calculator_eval(eq, cur, p + i, message);
-    if (isnan(p[i]) || (!isMinYNan && isless(p[i], *minY) ||
-                        (!isMaxYNan && isless(*maxY, p[i])))) {
-      p[i] = NAN;
+
+    if (isnan(p[i])) { // handling errors
       print_label_error(messages, message);
+    } else if ((!isMinYNan && isless(p[i], *minY) ||
+                (!isMaxYNan && isless(*maxY, p[i])))) {
+      // out-of-bounds' values should be NANs
+      p[i] = NAN;
     } else {
+      // otherwise, y values that are set to auto should be updated
       if (isMinYNan)
         *minY = fmin(*minY, p[i]);
       if (isMaxYNan)
         *maxY = fmax(*maxY, p[i]);
     }
   }
+
+  // equal limits makes the line appear as if there is no drawing
+  if (isgreaterequal(*minY, *maxY) && isgreaterequal(*maxY, *minY)) {
+    *minY -= 0.01;
+    *maxY += 0.01;
+  }
+
   gtk_widget_queue_draw(handler->graph);
 }
 
@@ -314,6 +377,7 @@ void gui_handler_start_gui(int argc, char *argv[]) {
 
   gui_handler handler;
 
+  // initializing variables
   handler.gridActive = handler.helperActive = TRUE;
   handler.helperX = WIDTH / 2.0 + HELPER_DX;
   handler.helperY = HEIGHT / 2.0 + HELPER_DY;
@@ -324,6 +388,7 @@ void gui_handler_start_gui(int argc, char *argv[]) {
   GtkBuilder *builder = gtk_builder_new_from_file("plotter.glade");
   gtk_builder_connect_signals(builder, NULL);
 
+  // getting widgets from glade file
   handler.window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
   handler.fixed = GTK_WIDGET(gtk_builder_get_object(builder, "fixed"));
   handler.graph = GTK_WIDGET(gtk_builder_get_object(builder, "graph"));
@@ -345,6 +410,7 @@ void gui_handler_start_gui(int argc, char *argv[]) {
 
   g_signal_connect(handler.window, "destroy", G_CALLBACK(on_destroy), NULL);
 
+  // passing the handler to every event
   g_signal_connect(handler.graph, "draw", G_CALLBACK(on_graph_draw), &handler);
   g_signal_connect(handler.draw, "clicked", G_CALLBACK(on_draw_clicked),
                    &handler);
@@ -367,6 +433,7 @@ void gui_handler_start_gui(int argc, char *argv[]) {
 
   gtk_main();
 
+  // freeing manually allocated memory
   if (handler.eq)
     free(handler.eq);
   free(handler.points);
